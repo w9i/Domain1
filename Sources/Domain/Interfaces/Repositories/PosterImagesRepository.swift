3438fe6890eb5483f92378951f7d31fe7888e9df
w9i <50361448+w9i@users.noreply.github.com>
2022-05-18 18:30:01 +0800

    Add files via upload

---
+++ Sources/Domain/Interfaces/Repositories/PosterImagesRepository.swift
@@ -0,0 +1,13 @@
+//
+//  PosterImagesRepositoryInterface.swift
+//  ExampleMVVM
+//
+//  Created by Oleh Kudinov on 01.10.18.
+//
+
+import Foundation
+import Common
+
+protocol PosterImagesRepository {
+    func fetchImage(with imagePath: String, width: Int, completion: @escaping (Result<Data, Error>) -> Void) -> Cancellable?
+}
