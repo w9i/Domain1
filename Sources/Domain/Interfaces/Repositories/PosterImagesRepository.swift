0e765181268e2447ed64e0345323a0a11e8f62f1
w9i <50361448+w9i@users.noreply.github.com>
2022-05-18 18:37:38 +0800

    Update PosterImagesRepository.swift

--- Sources/Domain/Interfaces/Repositories/PosterImagesRepository.swift
+++ Sources/Domain/Interfaces/Repositories/PosterImagesRepository.swift
@@ -8,6 +8,6 @@
 import Foundation
 import Common
 
-protocol PosterImagesRepository {
+public protocol PosterImagesRepository {
     func fetchImage(with imagePath: String, width: Int, completion: @escaping (Result<Data, Error>) -> Void) -> Cancellable?
 }
